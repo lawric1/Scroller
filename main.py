d867abc828cc607ce720d262f64387cb670993bd
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-08-25 21:30:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui
+import keyboard
+
+speed = 0
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        if keyboard.is_pressed('x'):
+            speed -= 3
+        elif keyboard.is_pressed('z'):
+            speed += 3
+        elif keyboard.is_pressed('ctrl'):
+            speed = 0
+
+        pyautogui.scroll(speed)
+except KeyboardInterrupt:
+    print('\n')
